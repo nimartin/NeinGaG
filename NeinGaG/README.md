63385ce8ac13c2a4fa41f3e42ea5a0f2120c6fa2
Yvan Betremieux <yvbetremie1@dpt_info.iut.local>
2017-05-22 12:01:47 +0200

    premier pas

---
+++ NeinGaG/README.md
@@ -0,0 +1,4 @@
+NeinGaG
+=======
+
+A Symfony project created on May 22, 2017, 11:44 am.
---
+++ NeinGaG/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        // return $this->render('default/index.html.twig', [
+        //     'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        // ]);
+
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+
+    public function numberAction()
+    {
+        
+    }
+}
